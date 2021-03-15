cff648edd8b2d359f2d727a26542d037f775f664
stackedsax <alex.scammon@gresearch.co.uk>
2021-03-15 14:51:58 -0700

    Checking in changes with minikube deployment

---
+++ deployment/minikube/minikube.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+profile=siembol
+
+echo == install prerequisites ==
+brew install helm kubernetes-cli minikube mkcert nss
+
+echo == create and install CA ==
+mkcert -install
+
+echo == create k8s cluster ==
+minikube start --profile $profile --driver hyperkit --container-runtime containerd --cni calico --cpus 8 --memory 16g --disk-size 40g --addons ingress,ingress-dns
+minikube profile $profile
+
+echo == add DNS resolver ==
+sudo tee /etc/resolver/minikube-$(minikube profile) >/dev/null << EOF
+domain $(minikube profile).local
+nameserver $(minikube ip)
+search_order 1
+timeout 5
+EOF
+
+echo == install cert-manager ==
+helm repo add jetstack https://charts.jetstack.io
+kubectl create namespace cert-manager
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.1.0 --set installCRDs=true
+
+
+printf '== wait for cert-manager to be up'
+for deployment in cert-manager cert-manager-cainjector cert-manager-webhook
+do
+  while ! [ "$(kubectl get deployment $deployment --namespace cert-manager -o jsonpath='{.status.readyReplicas}')" == "1" ]; do printf .; sleep 1; done
+done
+echo ' =='
+
+
+echo == install CA in siembol namespace ==
+kubectl create namespace siembol
+kubectl create secret tls ca-key-pair --namespace siembol --cert=$HOME/Library/ApplicationSupport/mkcert/rootCA.pem --key=$HOME/Library/ApplicationSupport/mkcert/rootCA-key.pem
+kubectl apply -f - <<EOF
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: ca-issuer
+  namespace: siembol
+spec:
+  ca:
+    secretName: ca-key-pair
+EOF
+
+
+
+
