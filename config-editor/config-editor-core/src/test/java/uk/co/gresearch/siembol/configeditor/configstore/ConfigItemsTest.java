04003a622e9c304f0b02a278d59eceb4d7302fa0
Marian Novotny <46998027+mariannovotny@users.noreply.github.com>
2021-05-10 14:10:45 +0100

    config editor: fixing updating cache (#50)

--- config-editor/config-editor-core/src/test/java/uk/co/gresearch/siembol/configeditor/configstore/ConfigItemsTest.java
+++ config-editor/config-editor-core/src/test/java/uk/co/gresearch/siembol/configeditor/configstore/ConfigItemsTest.java
@@ -15,10 +15,7 @@ import uk.co.gresearch.siembol.configeditor.git.GitRepository;
 import uk.co.gresearch.siembol.configeditor.model.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -39,10 +36,9 @@ public class ConfigItemsTest {
     private Map<String, String> filesContent = new HashMap<>();
 
     private List<ConfigEditorFile> files;
-    private List<ConfigEditorFile> transactFiles;
     private ConfigEditorResult getFilesResult;
-    private ConfigEditorResult transactGetFilesResult;
     private ConfigInfo configInfo = new ConfigInfo();
+    private ConfigInfo newConfigInfo = new ConfigInfo();
     private UserInfo user;
 
     @Before
@@ -52,15 +48,14 @@ public class ConfigItemsTest {
         configInfoProvider = Mockito.spy(ConfigInfoProvider.class);
 
         configInfo.setName("test");
-        configInfo.setVersion(1);
-        when(configInfoProvider.getConfigInfo(any(UserInfo.class), anyString())).thenReturn(configInfo);
+        configInfo.setOldVersion(1);
 
+        when(configInfoProvider.getConfigInfo(any(UserInfo.class), anyString())).thenReturn(configInfo);
         when(configInfoProvider.isStoreFile(anyString())).thenReturn(true);
         when(configInfoProvider.getFileContentType()).thenReturn(ConfigEditorFile.ContentType.STRING);
-        filesContent.put("File.json", "DUMMY_CONTENT");
+        filesContent.put("test.json", "DUMMY_CONTENT");
         files = new ArrayList<>();
-        files.add(new ConfigEditorFile("File.json",
-                "DUMMY_CONTENT",
+        files.add(new ConfigEditorFile("test.json", "DUMMY_CONTENT",
                 ConfigEditorFile.ContentType.RAW_JSON_STRING));
         ConfigEditorAttributes attr = new ConfigEditorAttributes();
         attr.setFiles(files);
@@ -71,17 +66,10 @@ public class ConfigItemsTest {
         when(gitRepo.getRepoUri()).thenReturn(dummyRepoUrl);
         when(gitRepo.getDirectoryUrl(eq(directory))).thenReturn(dummyDirectoryRepoUrl);
 
-        transactFiles = new ArrayList<>();
-        transactFiles.add(new ConfigEditorFile("Updated.json",
-                "DUMMY_CONTENT_UPDATED",
-                ConfigEditorFile.ContentType.RAW_JSON_STRING));
-        ConfigEditorAttributes attrTransact = new ConfigEditorAttributes();
-        attrTransact.setFiles(transactFiles);
-        transactGetFilesResult = new ConfigEditorResult(OK, attrTransact);
         when(gitRepo.transactCopyAndCommit(any(ConfigInfo.class),
                 eq(directory),
                 ArgumentMatchers.<Function<String, Boolean>>any()))
-                .thenReturn(transactGetFilesResult);
+                .thenReturn(getFilesResult);
 
         configItems = new ConfigItems(gitRepo, configInfoProvider, directory);
         user = new UserInfo();
@@ -113,7 +101,7 @@ public class ConfigItemsTest {
         Assert.assertNotNull(result.getAttributes().getFiles());
         Assert.assertEquals(1, result.getAttributes().getFiles().size());
         Assert.assertEquals(1, result.getAttributes().getFiles().size());
-        Assert.assertEquals("File.json", result.getAttributes().getFiles().get(0).getFileName());
+        Assert.assertEquals("test.json", result.getAttributes().getFiles().get(0).getFileName());
         Assert.assertEquals("DUMMY_CONTENT", result.getAttributes().getFiles().get(0).getContent());
     }
 
@@ -127,37 +115,45 @@ public class ConfigItemsTest {
 
     @Test
     public void addItemOK() throws IOException, GitAPIException {
-        configInfo.setOldVersion(0);
-        configInfo.setVersion(1);
-        configInfo.setName("new_config");
-        configInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        configItems.init();
+        Map<String, Optional<String>> fileContent = new HashMap<>();
+        fileContent.put("new_config.json", Optional.of("NEW_CONFIG"));
+        newConfigInfo.setOldVersion(0);
+        newConfigInfo.setVersion(1);
+        newConfigInfo.setName("new_config");
+        newConfigInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        newConfigInfo.setFilesContent(fileContent);
 
-        ConfigEditorResult result = configItems.addConfigItem(user, "DUMMY_CONTENT_UPDATED");
+        when(configInfoProvider.getConfigInfo(any(UserInfo.class), eq("NEW_CONFIG"))).thenReturn(newConfigInfo);
+
+        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_CONFIG");
+
         verify(configInfoProvider, times(1))
-                .getConfigInfo(user, "DUMMY_CONTENT_UPDATED");
+                .getConfigInfo(user, "NEW_CONFIG");
+
         verify(gitRepo, times(1))
-                .transactCopyAndCommit(eq(configInfo), eq(directory), any());
+                .transactCopyAndCommit(eq(newConfigInfo), eq(directory), any());
         Assert.assertEquals(OK, result.getStatusCode());
         Assert.assertNotNull(result.getAttributes().getFiles());
-        Assert.assertEquals("Updated.json", result.getAttributes().getFiles().get(0).getFileName());
-        Assert.assertEquals("DUMMY_CONTENT_UPDATED", result.getAttributes().getFiles().get(0).getContent());
-
-        ConfigEditorResult resultGetFiles = configItems.getFiles();
-        Assert.assertEquals("Updated.json", resultGetFiles.getAttributes().getFiles().get(0).getFileName());
-        Assert.assertEquals("DUMMY_CONTENT_UPDATED",
-                resultGetFiles.getAttributes().getFiles().get(0).getContent());
     }
 
     @Test
     public void addNewItemWrongVersion() throws IOException, GitAPIException {
-        configInfo.setOldVersion(1);
-        configInfo.setVersion(2);
-        configInfo.setName("new_config");
-        configInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        configItems.init();
+        Map<String, Optional<String>> fileContent = new HashMap<>();
+        fileContent.put("new_config.json", Optional.of("NEW_CONFIG"));
+        newConfigInfo.setOldVersion(1);
+        newConfigInfo.setVersion(2);
+        newConfigInfo.setName("new_config");
+        newConfigInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        newConfigInfo.setFilesContent(fileContent);
 
-        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_DUMMY_ITEM");
+        when(configInfoProvider.getConfigInfo(any(UserInfo.class), eq("NEW_CONFIG"))).thenReturn(newConfigInfo);
+
+        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_CONFIG");
+
         verify(configInfoProvider, times(1))
-                .getConfigInfo(user, "NEW_DUMMY_ITEM");
+                .getConfigInfo(user, "NEW_CONFIG");
         verify(gitRepo, times(0))
                 .transactCopyAndCommit(any(), eq(directory), any());
         Assert.assertEquals(BAD_REQUEST, result.getStatusCode());
@@ -165,67 +161,51 @@ public class ConfigItemsTest {
     }
 
     @Test
-    public void addAndUpdateItemOK() throws IOException, GitAPIException {
-        configInfo.setOldVersion(0);
-        configInfo.setVersion(1);
-        configInfo.setName("test_config");
-        configInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+    public void updateItemOK() throws IOException, GitAPIException {
+        configItems.init();
+        Map<String, Optional<String>> fileContent = new HashMap<>();
+        fileContent.put("test.json", Optional.of("DUMMY_CONTENT_UPDATE"));
+        newConfigInfo.setOldVersion(1);
+        newConfigInfo.setVersion(2);
+        newConfigInfo.setName("test");
+        newConfigInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        newConfigInfo.setFilesContent(fileContent);
 
-        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_DUMMY_ITEM");
+        when(configInfoProvider.getConfigInfo(any(UserInfo.class), eq("DUMMY_CONTENT_UPDATE")))
+                .thenReturn(newConfigInfo);
+
+        ConfigEditorResult result = configItems.updateConfigItem(user, "DUMMY_CONTENT_UPDATE");
         verify(configInfoProvider, times(1))
-                .getConfigInfo(user, "NEW_DUMMY_ITEM");
-
-        Assert.assertEquals(OK, result.getStatusCode());
-        Assert.assertNotNull(result.getAttributes().getFiles());
-
-        configInfo.setOldVersion(1);
-        configInfo.setVersion(2);
-
-        result = configItems.updateConfigItem(user, "NEW_DUMMY_ITEM_UPDATE");
+                .getConfigInfo(eq(user), eq("DUMMY_CONTENT_UPDATE"));
         verify(configInfoProvider, times(1))
-                .getConfigInfo(user, "NEW_DUMMY_ITEM_UPDATE");
+                .getConfigInfo(any(UserInfo.class), eq("DUMMY_CONTENT"));
+        verify(gitRepo, times(1))
+                .transactCopyAndCommit(any(), eq(directory), any());
         Assert.assertEquals(OK, result.getStatusCode());
         Assert.assertNotNull(result.getAttributes().getFiles());
     }
 
     @Test
-    public void addAndUpdateItemWrongVersion() throws IOException, GitAPIException {
-        configInfo.setOldVersion(0);
-        configInfo.setVersion(1);
-        configInfo.setName("test_config");
-        configInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+    public void updateItemWrongVersion() throws IOException, GitAPIException {
+        configItems.init();
+        Map<String, Optional<String>> fileContent = new HashMap<>();
+        fileContent.put("test.json", Optional.of("DUMMY_CONTENT_UPDATE"));
+        newConfigInfo.setOldVersion(2);
+        newConfigInfo.setVersion(3);
+        newConfigInfo.setName("test");
+        newConfigInfo.setConfigInfoType(ConfigInfoType.CONFIG);
+        newConfigInfo.setFilesContent(fileContent);
 
-        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_DUMMY_ITEM");
-        Assert.assertEquals(OK, result.getStatusCode());
-        Assert.assertNotNull(result.getAttributes().getFiles());
+        when(configInfoProvider.getConfigInfo(any(UserInfo.class), eq("DUMMY_CONTENT_UPDATE")))
+                .thenReturn(newConfigInfo);
 
-        configInfo.setOldVersion(0);
-        configInfo.setVersion(1);
-
-        result = configItems.updateConfigItem(user, "NEW_DUMMY_ITEM_UPDATE");
-        Assert.assertEquals(BAD_REQUEST, result.getStatusCode());
-        Assert.assertNotNull(result.getAttributes().getMessage());
-    }
-
-    @Test
-    public void addAndUpdateAndUpdateItemWrongOK() throws IOException, GitAPIException {
-        configInfo.setOldVersion(0);
-        configInfo.setVersion(1);
-        configInfo.setName("test_config");
-        configInfo.setConfigInfoType(ConfigInfoType.CONFIG);
-
-        ConfigEditorResult result = configItems.addConfigItem(user, "NEW_DUMMY_ITEM");
-        Assert.assertEquals(OK, result.getStatusCode());
-        Assert.assertNotNull(result.getAttributes().getFiles());
-
-        configInfo.setOldVersion(1);
-        configInfo.setVersion(2);
-
-        result = configItems.updateConfigItem(user, "NEW_DUMMY_ITEM_UPDATE");
-        Assert.assertEquals(OK, result.getStatusCode());
-        Assert.assertNotNull(result.getAttributes().getFiles());
-
-        result = configItems.updateConfigItem(user, "NEW_DUMMY_ITEM_UPDATE_2");
+        ConfigEditorResult result = configItems.updateConfigItem(user, "DUMMY_CONTENT_UPDATE");
+        verify(configInfoProvider, times(1))
+                .getConfigInfo(eq(user), eq("DUMMY_CONTENT_UPDATE"));
+        verify(configInfoProvider, times(1))
+                .getConfigInfo(any(UserInfo.class), eq("DUMMY_CONTENT"));
+        verify(gitRepo, times(0))
+                .transactCopyAndCommit(any(), eq(directory), any());
         Assert.assertEquals(BAD_REQUEST, result.getStatusCode());
         Assert.assertNotNull(result.getAttributes().getMessage());
     }
@@ -236,13 +216,13 @@ public class ConfigItemsTest {
         when(configInfoProvider.getConfigInfoType()).thenReturn(ConfigInfoType.CONFIG);
         verify(gitRepo, times(1))
                 .getFiles(eq(directory), ArgumentMatchers.<Function<String, Boolean>>any());
-        ConfigEditorResult result = configItems.deleteItems(user, "File.json");
+        ConfigEditorResult result = configItems.deleteItems(user, "test.json");
         Assert.assertEquals(OK, result.getStatusCode());
     }
 
     @Test
     public void deleteTwoFilesOK() throws IOException, GitAPIException {
-        files.add(new ConfigEditorFile("File2.json",
+        files.add(new ConfigEditorFile("test2.json",
                 "DUMMY_CONTENT",
                 ConfigEditorFile.ContentType.RAW_JSON_STRING));
 
@@ -250,7 +230,7 @@ public class ConfigItemsTest {
         when(configInfoProvider.getConfigInfoType()).thenReturn(ConfigInfoType.CONFIG);
         verify(gitRepo, times(1))
                 .getFiles(eq(directory), ArgumentMatchers.<Function<String, Boolean>>any());
-        ConfigEditorResult result = configItems.deleteItems(user, "File");
+        ConfigEditorResult result = configItems.deleteItems(user, "test");
         Assert.assertEquals(OK, result.getStatusCode());
     }
 
@@ -265,7 +245,7 @@ public class ConfigItemsTest {
                 eq(directory),
                 ArgumentMatchers.<Function<String, Boolean>>any()))
                 .thenReturn(ConfigEditorResult.fromMessage(ERROR, "error"));
-        ConfigEditorResult result = configItems.deleteItems(user, "File.json");
+        ConfigEditorResult result = configItems.deleteItems(user, "test.json");
         Assert.assertEquals(ERROR, result.getStatusCode());
     }
 }
