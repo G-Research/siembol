7d3132c5efecbda7e8afdf3ffdfd6abe5afd1056
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2021-06-10 14:04:12 +0100

    fixed ImportConfigRequestDto

--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ImportConfigRequestDto.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ImportConfigRequestDto.java
@@ -1,7 +1,10 @@
 package uk.co.gresearch.siembol.configeditor.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class ImportConfigRequestDto {
     @JsonProperty("importer_name")
@@ -16,6 +19,12 @@ public class ImportConfigRequestDto {
         return importerAttributes;
     }
 
+    @JsonSetter("importer_attributes")
+    public void setImporterAttributes(JsonNode node) {
+        this.importerAttributes = node.toString();
+    }
+
+    @JsonIgnore
     public void setImporterAttributes(String importerAttributes) {
         this.importerAttributes = importerAttributes;
     }
