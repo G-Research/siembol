bd6d378368b404da74eae0162f6e5357bae7f9b2
Yassin Raman <yassin@gmail.co>
2021-07-12 21:09:46 +0100

    minikube helm k8s deployment

---
+++ deployment/helm-k8s/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+CONFIG_MAP_NAME_GIT="github-details"
+GIT_SECRET_NAME="siembol-config-editor-rest-secrets"
+NAMESPACE="siembol"
+
+echo "************** First Iteration Install Script **************"
+echo "************************************************************"
+read -p 'Github username: ' GIT_USERNAME
+read -p 'Github URL: ' GIT_URL
+read -p 'Github repo name: ' GIT_REPO_NAME
+read -p 'Github personal token: ' GIT_TOKEN
+
+echo "You entered these details: "
+echo "Github username: $GIT_USERNAME"
+echo "Github URL: $GIT_URL"
+echo "Github repo name: $GIT_REPO_NAME"
+echo "Github personal token: $GIT_TOKEN"
+
+read -p "Are these details correct? (yes/no)" choice
+echo $choice
+if [ $choice = 'yes' ]; then
+    echo "creating configmap"
+    kubectl create configmap $CONFIG_MAP_NAME_GIT -n $NAMESPACE --from-literal=GITHUB_USER=$GIT_USERNAME --from-literal=GITHUB_URL=$GIT_URL --from-literal=GITHUB_REPO_NAME=$GIT_REPO_NAME 
+    sleep 3
+    echo "Creating github secret"
+    kubectl create secret generic $GIT_SECRET_NAME -n $NAMESPACE --from-literal=git=$GIT_TOKEN
+    sleep 3
+else
+    echo "Run script again"
+    exit 1
+
+fi
+
+echo "************************************************************"
+echo "******  You can now deploy siembol from helm charts   ******"
+echo "************************************************************"
