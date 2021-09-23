d0d8469ab22dd80ed3b99b2ace7831e0afd42761
Marian Novotny <46998027+mariannovotny@users.noreply.github.com>
2021-09-23 11:43:53 +0100

    Revert kafka clients in storm to version 0.10.2.2 (#327)
    * Revert "Upgrading curator, zookeeper and kafka (#322)"
    
    This reverts commit e7659cfde08421a311ef61329ee655f014eb01a8.
    
    * using kafka clients 0.10.2.2
    
    * use nod version 12.22.5

--- enriching/enriching-storm/src/test/java/uk/co/gresearch/siembol/enrichments/storm/StormEnrichingApplicationTest.java
+++ enriching/enriching-storm/src/test/java/uk/co/gresearch/siembol/enrichments/storm/StormEnrichingApplicationTest.java
@@ -232,7 +232,6 @@ public class StormEnrichingApplicationTest {
         cluster.submitTopology("test", config, topology);
     }
 
-    @Ignore
     @Test(timeout=200000)
     public void testEnrichMessageOK() throws Exception {
         kafkaRule.helper().produceStrings("enrichments", event.trim());
