8671dff7575ef5c053745ee8f646beff69db64fa
Marian Novotny <46998027+mariannovotny@users.noreply.github.com>
2021-10-04 16:53:09 +0100

    Revert "Update storm-entrypoint.sh (#360)" (#361)
    This reverts commit 6105a1ec2bcfd0031e600639cfc6e8aa0bb72a8b.

--- deployment/docker/storm-entrypoint.sh
+++ deployment/docker/storm-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # if command starts with something that is not executable, prepend our deploy command
 if ! which "${1}" >/dev/null; then
-  set -- storm jar $TOPOLOGY_JAR $TOPOLOGY_CLASS "$@" -c nimbus.seeds="${NIMBUS_SEEDS:-"[\"nimbus\"]"}" -c nimbus.thrift.port=${NIMBUS_PORT:-6627} -u siembol
+  set -- storm jar $TOPOLOGY_JAR $TOPOLOGY_CLASS "$@" -c nimbus.seeds="${NIMBUS_SEEDS:-"[\"nimbus\"]"}" -c nimbus.thrift.port=${NIMBUS_PORT:-6627}
 fi
 
 exec "$@"
