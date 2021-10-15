0f43a785b017067e2c368eac06aee0b56482aa73
yasram1 <yasram1@github.com>
2021-10-15 17:58:21 +0100

    small fixes

--- deployment/enrichment/download.php
+++ deployment/enrichment/download.php
@@ -1,7 +1,6 @@
 <?php
-parse_str($_SERVER['QUERY_STRING'], $output);
-$filename = $output['filename']; 
-echo $filename;
+parse_str($_SERVER['QUERY_STRING'], $output); //TODO: Need some input validation
+$filename = $output['filename'];
 $path='/opt/files/' . $filename;
 echo file_get_contents($path);
 ?>
