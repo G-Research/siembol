e1b470a281c7b99a5fa0acacd485e0de6f4bcaff
yasram1 <yasram1@github.com>
2021-10-15 17:56:00 +0100

    not needed

---
+++ deployment/enrichment/upload.php
@@ -0,0 +1,17 @@
+<?PHP
+if(!empty($_FILES['uploaded_file'])) {
+  {
+    $path = "/opt/files";
+    $filename = basename( $_FILES['uploaded_file']['name']);
+
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], "$path/$filename")) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+  }
+} else {
+    echo "Please specify a file with correct key; 'uploaded_file=FILENAME'"
+}
+?>
