488be2a1f9d2a5092590ba804508a0400bca2b67
cmv13 <cvalentiny@hotmail.fr>
2021-12-20 15:54:52 +0000

    upgrade

--- config-editor/config-editor-ui/src/test.ts
+++ config-editor/config-editor-ui/src/test.ts
@@ -14,7 +14,9 @@ import {
 declare const require: any;
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false }
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
