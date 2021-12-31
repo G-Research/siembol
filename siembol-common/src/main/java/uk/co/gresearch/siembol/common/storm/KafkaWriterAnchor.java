ad352f1695c10bf30c1a8e18d10648253488fe44
Marian Novotny <46998027+mariannovotny@users.noreply.github.com>
2021-12-31 11:53:59 +0000

    Siembol common: Reworking kafka writer to send asynchronous  messages (#470)
    * REworking kafka writer to asynchronous sending messages
    
    * increasing siembol version to 2.2.4-SNAPSHOT

---
+++ siembol-common/src/main/java/uk/co/gresearch/siembol/common/storm/KafkaWriterAnchor.java
@@ -0,0 +1,26 @@
+package uk.co.gresearch.siembol.common.storm;
+
+import org.apache.storm.tuple.Tuple;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class KafkaWriterAnchor {
+    private final Tuple tuple;
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    public KafkaWriterAnchor(Tuple tuple) {
+        this.tuple = tuple;
+    }
+
+    public void acquire() {
+        counter.incrementAndGet();
+    }
+
+    public boolean release() {
+        return counter.decrementAndGet() <= 0;
+    }
+
+    public Tuple getTuple() {
+        return tuple;
+    }
+}
