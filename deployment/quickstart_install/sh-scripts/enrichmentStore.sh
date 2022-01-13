58d45e3809f6bb2cd4fb413c3d84d0ff9f3b48ff
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2022-01-13 15:07:55 +0100

    [quickstart] fix enrichmentStore scripts as well

--- deployment/quickstart_install/sh-scripts/enrichmentStore.sh
+++ deployment/quickstart_install/sh-scripts/enrichmentStore.sh
@@ -2,13 +2,13 @@ echo "********* Set up demo enrichment table **************"
 echo "*****************************************************"
 
 echo '{"1.2.3.4":{"hostname":"test-name"}}' > hostname.json
-curl -F "uploaded_file=@hostname.json;" https://enrichment.local/upload.php
+curl -F "uploaded_file=@hostname.json;" https://enrichment.siembol.local/upload.php
 
 curl -X 'POST' https://rest.siembol.local/api/v1/enrichment/enrichment/tables -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"name\": \"hostname\",\"path\": \"/download.php?filename=dns.json\"}'
 
 echo "************************************************************"
 echo "Check uploaded table through this url in the browser:"
-echo "https://enrichment.local/download.php?filename=hostname.json"
+echo "https://enrichment.siembol.local/download.php?filename=hostname.json"
 echo "************************************************************"
 echo "Check siembol table info through this url in the browser:"
 echo "https://rest.siembol.local/api/v1/enrichment/enrichment/tables"
