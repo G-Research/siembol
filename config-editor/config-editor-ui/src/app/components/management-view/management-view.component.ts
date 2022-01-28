c2d59619807c60f1a92a8e679b89f52471df1c2c
cmv13 <cvalentiny@hotmail.fr>
2022-01-28 15:11:35 +0000

    fix loading apps

--- config-editor/config-editor-ui/src/app/components/management-view/management-view.component.ts
+++ config-editor/config-editor-ui/src/app/components/management-view/management-view.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { HelpLink } from "@app/model/app-config";
 import { AppConfigService } from "@app/services/app-config.service";
+import { AppService } from "@app/services/app.service";
 import { ApplicationDialogComponent } from "..";
 
 @Component({
@@ -16,7 +17,8 @@ export class ManagementViewComponent  implements OnInit {
 
   constructor(
     private appConfigService: AppConfigService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private service: AppService
   ) {}
 
   ngOnInit() {
@@ -28,6 +30,8 @@ export class ManagementViewComponent  implements OnInit {
   }
 
   openApplicationDialog() {
-    this.dialog.open(ApplicationDialogComponent);
+    this.service.getAllApplications().subscribe(apps => 
+      this.dialog.open(ApplicationDialogComponent, { data: apps })
+    );
   }
 }
