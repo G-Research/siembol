a7ff502a41274fa82a3ea0ed20a247e03b0372af
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-03-01 15:20:59 +0000

    fixing description

--- responding/responding-core/src/main/java/uk/co/gresearch/siembol/response/model/KafkaWriterEvaluatorAttributesDto.java
+++ responding/responding-core/src/main/java/uk/co/gresearch/siembol/response/model/KafkaWriterEvaluatorAttributesDto.java
@@ -3,7 +3,7 @@ package uk.co.gresearch.siembol.response.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.reinert.jjschema.Attributes;
 
-@Attributes(title = "kafka writer evaluator attributes", description = "Attributes for sleep evaluator")
+@Attributes(title = "kafka writer evaluator attributes", description = "Attributes for kafka writer evaluator")
 public class KafkaWriterEvaluatorAttributesDto {
     @JsonProperty("topic_name")
     @Attributes(required = true, description = "The name of the kafka topic")
