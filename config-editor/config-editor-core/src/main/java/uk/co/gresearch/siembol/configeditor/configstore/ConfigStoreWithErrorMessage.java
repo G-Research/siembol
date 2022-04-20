1fd1536714868645465a71c3f985e5c74778349a
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-04-20 14:38:23 +0100

    work in progress

--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/configstore/ConfigStoreWithErrorMessage.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/configstore/ConfigStoreWithErrorMessage.java
@@ -3,6 +3,9 @@ package uk.co.gresearch.siembol.configeditor.configstore;
 import org.springframework.boot.actuate.health.Health;
 import uk.co.gresearch.siembol.configeditor.common.UserInfo;
 import uk.co.gresearch.siembol.configeditor.model.ConfigEditorResult;
+import uk.co.gresearch.siembol.configeditor.model.ErrorTitles;
+
+import java.util.function.Supplier;
 
 public class ConfigStoreWithErrorMessage implements ConfigStore {
     private final ConfigStore configStore;
@@ -13,23 +16,21 @@ public class ConfigStoreWithErrorMessage implements ConfigStore {
 
     @Override
     public ConfigEditorResult addTestCase(UserInfo user, String testCase) {
-        var ret = configStore.addTestCase(user, testCase);
-        if (ret.getStatusCode() == ConfigEditorResult.StatusCode.BAD_REQUEST) {
-            
-        }
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.addTestCase(user, testCase);
+        return executeInternally(fun, ErrorTitles.ADD_TEST_CASE.getTitle(),
+                null, null);
     }
 
     @Override
     public ConfigEditorResult updateTestCase(UserInfo user, String testCase) {
-        var ret = configStore.updateTestCase(user, testCase);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.updateTestCase(user, testCase);
+        return executeInternally(fun, ErrorTitles.UPDATE_TEST_CASE.getTitle(), null, null);
     }
 
     @Override
     public ConfigEditorResult deleteTestCase(UserInfo user, String configName, String testCaseName) {
-        var ret = configStore.deleteTestCase(user, configName, testCaseName);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.deleteTestCase(user, configName, testCaseName);
+        return executeInternally(fun, null, null, null);
     }
 
     @Override
@@ -39,20 +40,20 @@ public class ConfigStoreWithErrorMessage implements ConfigStore {
 
     @Override
     public ConfigEditorResult addConfig(UserInfo user, String newConfig) {
-        var ret = configStore.addConfig(user, newConfig);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.addConfig(user, newConfig);
+        return executeInternally(fun, null, null, null);
     }
 
     @Override
     public ConfigEditorResult updateConfig(UserInfo user, String configToUpdate) {
-        var ret = configStore.updateConfig(user, configToUpdate);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.addConfig(user, configToUpdate);
+        return executeInternally(fun, null, null, null);
     }
 
     @Override
     public ConfigEditorResult deleteConfig(UserInfo user, String configName) {
-        var ret = configStore.deleteConfig(user, configName);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.addConfig(user, configName);
+        return executeInternally(fun, null, null, null);
     }
 
     @Override
@@ -77,23 +78,23 @@ public class ConfigStoreWithErrorMessage implements ConfigStore {
 
     @Override
     public ConfigEditorResult submitConfigsRelease(UserInfo user, String rulesRelease) {
-        var ret = configStore.submitConfigsRelease(user, rulesRelease);
-        return ret;
+        Supplier<ConfigEditorResult> fun = () -> configStore.submitConfigsRelease(user, rulesRelease);
+        return executeInternally(fun, null, null, null);
     }
 
     @Override
     public ConfigEditorResult getAdminConfigFromCache() {
-        return null;
+        return configStore.getAdminConfigFromCache();
     }
 
     @Override
     public ConfigEditorResult getAdminConfig() {
-        return null;
+        return configStore.getAdminConfig();
     }
 
     @Override
     public ConfigEditorResult getAdminConfigStatus() {
-        return null;
+        return configStore.getAdminConfigStatus();
     }
 
     @Override
@@ -103,11 +104,25 @@ public class ConfigStoreWithErrorMessage implements ConfigStore {
 
     @Override
     public ConfigEditorResult getRepositories() {
-        return null;
+        return configStore.getRepositories();
     }
 
     @Override
     public Health checkHealth() {
-        return null;
+        return configStore.checkHealth();
+    }
+
+    private ConfigEditorResult executeInternally(Supplier<ConfigEditorResult> supplier,
+                                                 String title,
+                                                 String message,
+                                                 String resolution) {
+        var ret = supplier.get();
+        if (ret.getStatusCode() == ConfigEditorResult.StatusCode.BAD_REQUEST) {
+            var attributes = ret.getAttributes();
+            attributes.setErrorTitleIfNotPresent(title);
+            attributes.setMessageIfNotPresent(message);
+            attributes.setErrorResolutionIfNotPresent(message);
+        }
+        return ret;
     }
 }
--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorTitles.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorTitles.java
@@ -1,14 +1,17 @@
 package uk.co.gresearch.siembol.configeditor.model;
 
 public enum ErrorTitles {
-    ;
+    ADD_CONFIG("Problem during storing configuration in git repository"),
+    ADD_TEST_CASE("Problem during storing test case in git repository"),
+    UPDATE_CONFIG("Problem during updating configuration in git repository"),
+    UPDATE_TEST_CASE("Problem during updating test case in git repository");
     private final String title;
 
     ErrorTitles(String title) {
         this.title = title;
     }
 
-    public String getTitle(String ...args) {
+    public String getTitle(Object ...args) {
         return String.format(title, args);
     }
 }
