c93bcd37fc40bc44717b9f93c5ddbf01d35b4360
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-04-21 11:31:43 +0100

    adding missing files

---
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/common/ConfigSchemaServiceWithErrorMessage.java
@@ -0,0 +1,101 @@
+package uk.co.gresearch.siembol.configeditor.common;
+
+import org.springframework.boot.actuate.health.Health;
+import uk.co.gresearch.siembol.configeditor.model.ConfigEditorResult;
+import uk.co.gresearch.siembol.configeditor.model.ErrorMessages;
+import uk.co.gresearch.siembol.configeditor.model.ErrorResolutions;
+import uk.co.gresearch.siembol.configeditor.model.ErrorTitles;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ConfigSchemaServiceWithErrorMessage extends ServiceWithErrorMessage<ConfigSchemaService>
+        implements ConfigSchemaService {
+
+    public ConfigSchemaServiceWithErrorMessage(ConfigSchemaService service) {
+        super(service);
+    }
+
+    @Override
+    public ConfigEditorResult getSchema() {
+        return service.getSchema();
+    }
+
+    @Override
+    public ConfigEditorResult validateConfiguration(String configuration) {
+        Supplier<ConfigEditorResult> fun = () -> service.validateConfiguration(configuration);
+        return executeInternally(fun, ErrorTitles.VALIDATION_GENERIC.getTitle(),
+                ErrorMessages.VALIDATION_GENERIC.getMessage(),
+                ErrorResolutions.VALIDATION.getResolution());
+    }
+
+    @Override
+    public ConfigEditorResult validateConfigurations(String configurations) {
+        Supplier<ConfigEditorResult> fun = () -> service.validateConfigurations(configurations);
+        return executeInternally(fun, ErrorTitles.VALIDATION_GENERIC.getTitle(),
+                ErrorMessages.VALIDATION_GENERIC.getMessage(),
+                ErrorResolutions.VALIDATION.getResolution());
+    }
+
+    @Override
+    public Map<String, ConfigImporter> getConfigImporters() {
+        return service.getConfigImporters();
+    }
+
+    @Override
+    public Health checkHealth() {
+        return service.checkHealth();
+    }
+
+    @Override
+    public ConfigEditorResult getImporters() {
+        return service.getImporters();
+    }
+
+    @Override
+    public ConfigEditorResult importConfig(UserInfo user,
+                                           String importerName,
+                                           String importerAttributes,
+                                           String configToImport) {
+        return ConfigSchemaService.super.importConfig(user, importerName, importerAttributes, configToImport);
+    }
+
+    @Override
+    public ConfigEditorResult getTestSchema() {
+        return service.getTestSchema();
+    }
+
+    @Override
+    public ConfigEditorResult testConfiguration(String configuration, String testSpecification) {
+        Supplier<ConfigEditorResult> fun = () -> service.testConfiguration(configuration, testSpecification);
+        return executeInternally(fun, ErrorTitles.TESTING_GENERIC.getTitle(),
+                ErrorMessages.TESTING_GENERIC.getMessage(),
+                ErrorResolutions.GENERIC_BAD_REQUEST.getResolution());
+    }
+
+    @Override
+    public ConfigEditorResult testConfigurations(String configurations, String event) {
+        Supplier<ConfigEditorResult> fun = () -> service.testConfigurations(configurations, event);
+        return executeInternally(fun, ErrorTitles.VALIDATION_GENERIC.getTitle(),
+                ErrorMessages.VALIDATION_GENERIC.getMessage(),
+                ErrorResolutions.GENERIC_BAD_REQUEST.getResolution());
+    }
+
+    @Override
+    public ConfigEditorResult getAdminConfigurationSchema() {
+        return service.getAdminConfigurationSchema();
+    }
+
+    @Override
+    public ConfigEditorResult validateAdminConfiguration(String configuration) {
+        Supplier<ConfigEditorResult> fun = () -> service.validateAdminConfiguration(configuration);
+        return executeInternally(fun, ErrorTitles.VALIDATION_GENERIC.getTitle(),
+                ErrorMessages.VALIDATION_GENERIC.getMessage(),
+                ErrorResolutions.VALIDATION.getResolution());
+    }
+
+    @Override
+    public ConfigEditorResult getAdminConfigTopologyName(String configuration) {
+        return service.getAdminConfigTopologyName(configuration);
+    }
+}
