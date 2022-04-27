4945ed82f370102fd2a2b75e4fda31404a742265
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-04-27 12:28:19 +0100

    minor rewording

--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorMessages.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorMessages.java
@@ -3,8 +3,8 @@ package uk.co.gresearch.siembol.configeditor.model;
 public enum ErrorMessages {
     CONFIG_ITEM_ALREADY_EXISTS("%s already exists"),
     CONFIG_ITEM_UNEXPECTED_VERSION("Unexpected version for %s update"),
-    PR_PENDING("Can not create PR for %s because another PR: %s is pending in git repository"),
-    PR_UNEXPECTED_VERSION("Can not create PR for %s with the version %d. Expected version: %d"),
+    PR_PENDING("Cannot create PR for %s because another PR: %s is pending in git repository"),
+    PR_UNEXPECTED_VERSION("Cannot create PR for %s with the version %d. Expected version: %d"),
     GENERIC_BAD_REQUEST("The request is invalid"),
     VALIDATION_GENERIC("Attempt to validate changes failed"),
     TESTING_GENERIC("Attempt to test configuration failed"),
--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorResolutions.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorResolutions.java
@@ -1,10 +1,10 @@
 package uk.co.gresearch.siembol.configeditor.model;
 
 public enum ErrorResolutions {
-    GENERIC_BAD_REQUEST("Try to inspect error message and try to fix and replay your request"),
+    GENERIC_BAD_REQUEST("Inspect error message and try to fix and replay your request"),
     CONCURRENT_USERS("Siembol UI can be used by multiple users in parallel. " +
-            "Try to reload UI, modify and replay your request"),
-    VALIDATION("Try to inspect error message and modify your changes. " +
+            "Refresh UI (F5), modify and replay your request"),
+    VALIDATION("Inspect error message and modify your changes. " +
             "Ask administrators for help if the problem persists"),
     GENERIC_INTERNAL_ERROR("Ask administrators for help");
     private final String resolution;
