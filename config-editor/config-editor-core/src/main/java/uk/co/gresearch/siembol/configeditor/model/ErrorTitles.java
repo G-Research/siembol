840b17a6711b8f6439acb54c43d4c2a2e22a98d7
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-04-27 12:04:25 +0100

    change error titles

--- config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorTitles.java
+++ config-editor/config-editor-core/src/main/java/uk/co/gresearch/siembol/configeditor/model/ErrorTitles.java
@@ -1,14 +1,14 @@
 package uk.co.gresearch.siembol.configeditor.model;
 
 public enum ErrorTitles {
-    ADD_CONFIG("Problem during storing configuration in git repository"),
-    ADD_TEST_CASE("Problem during storing test case in git repository"),
-    UPDATE_CONFIG("Problem during updating configuration in git repository"),
-    UPDATE_TEST_CASE("Problem during updating test case in git repository"),
-    CREATE_RELEASE_PR("Problem during creating release PR in git repository"),
-    CREATE_ADMIN_CONFIG_PR("Problem during creating admin config PR in git repository"),
-    DELETE_CONFIG("Problem during deleting config %s in git repository"),
-    DELETE_TEST_CASE("Problem during deleting test case %s in git repository"),
+    ADD_CONFIG("Problem storing configuration in git repository"),
+    ADD_TEST_CASE("Problem storing test case in git repository"),
+    UPDATE_CONFIG("Problem updating configuration in git repository"),
+    UPDATE_TEST_CASE("Problem updating test case in git repository"),
+    CREATE_RELEASE_PR("Problem creating release PR in git repository"),
+    CREATE_ADMIN_CONFIG_PR("Problem creating admin config PR in git repository"),
+    DELETE_CONFIG("Problem deleting config %s in git repository"),
+    DELETE_TEST_CASE("Problem deleting test case %s in git repository"),
     VALIDATION_GENERIC("Validation failure"),
     TESTING_GENERIC("Testing failure"),
     IMPORTING_CONFIG_GENERIC("Importing configuration failed"),
