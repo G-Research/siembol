d71b81f3b851a03ddc1ee12f0628a3aa1c63940e
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-05-17 15:19:47 +0100

    adding time exclusion evaluator into siembol response

---
+++ responding/responding-core/src/main/java/uk/co/gresearch/siembol/response/evaluators/timeexclusion/TimeExclusionEvaluator.java
@@ -0,0 +1,65 @@
+package uk.co.gresearch.siembol.response.evaluators.timeexclusion;
+
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+import uk.co.gresearch.siembol.common.utils.EvaluationLibrary;
+import uk.co.gresearch.siembol.response.common.Evaluable;
+import uk.co.gresearch.siembol.response.common.RespondingResult;
+import uk.co.gresearch.siembol.response.common.ResponseAlert;
+import uk.co.gresearch.siembol.response.common.ResponseEvaluationResult;
+import uk.co.gresearch.siembol.response.model.TimeExclusionEvaluatorAttributesDto;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+
+public class TimeExclusionEvaluator implements Evaluable {
+    private static final String SUBSTITUTION_FAILED_MSG = "Substitution of time exclusion evaluator failed";
+    private static final String EMPTY_FIELDS_IN_ATTRIBUTES_MSG = "Empty fields in time exclusion evaluator attributes";
+    private static final String MISSING_TIMESTAMP_MSG = "Missing timestamp in alert";
+    private final TimeExclusionEvaluatorAttributesDto attributes;
+
+    public TimeExclusionEvaluator(TimeExclusionEvaluatorAttributesDto attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public RespondingResult evaluate(ResponseAlert alert) {
+        try {
+            var substituted = EvaluationLibrary.cloneAndSubstituteBean(attributes, alert);
+            if (substituted.isEmpty()
+                    || !(substituted.get() instanceof TimeExclusionEvaluatorAttributesDto)) {
+                throw new IllegalStateException(SUBSTITUTION_FAILED_MSG);
+            }
+
+            TimeExclusionEvaluatorAttributesDto currentAttr = (TimeExclusionEvaluatorAttributesDto) substituted.get();
+            if (Strings.isNullOrEmpty(currentAttr.getTimestampField())
+                    || Strings.isNullOrEmpty(currentAttr.getHoursOfDayPattern())
+                    || Strings.isNullOrEmpty(currentAttr.getDaysOfWeekPattern())
+                    || Strings.isNullOrEmpty(currentAttr.getMonthsOfYearPattern())) {
+                throw new IllegalStateException(EMPTY_FIELDS_IN_ATTRIBUTES_MSG);
+            }
+
+            Object timestampObj = alert.get(currentAttr.getTimestampField());
+            if (!(timestampObj instanceof Number)) {
+                throw new IllegalStateException(MISSING_TIMESTAMP_MSG);
+            }
+
+            Number timestamp = (Number) timestampObj;
+            Instant instant = Instant.ofEpochMilli(timestamp.longValue());
+            ZoneId zoneId = ZoneId.of(currentAttr.getTimeZone());
+            ZonedDateTime dateTime = ZonedDateTime.ofInstant(instant, zoneId);
+
+            if (String.valueOf(dateTime.getHour()).matches(currentAttr.getHoursOfDayPattern())
+                    && String.valueOf(dateTime.getDayOfWeek().getValue()).matches(currentAttr.getDaysOfWeekPattern())
+                    && String.valueOf(dateTime.getMonthValue()).matches(currentAttr.getMonthsOfYearPattern())) {
+                return RespondingResult.fromEvaluationResult(ResponseEvaluationResult.FILTERED, alert);
+            }
+
+            return RespondingResult.fromEvaluationResult(ResponseEvaluationResult.NO_MATCH, alert);
+        } catch (Exception e) {
+            return RespondingResult.fromException(e);
+        }
+    }
+}
---
+++ responding/responding-core/src/main/java/uk/co/gresearch/siembol/response/evaluators/timeexclusion/TimeExclusionEvaluatorFactory.java
@@ -0,0 +1,46 @@
+package uk.co.gresearch.siembol.response.evaluators.timeexclusion;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import uk.co.gresearch.siembol.common.jsonschema.SiembolJsonSchemaValidator;
+import uk.co.gresearch.siembol.common.result.SiembolResult;
+import uk.co.gresearch.siembol.response.common.ProvidedEvaluators;
+import uk.co.gresearch.siembol.response.common.RespondingEvaluatorFactory;
+import uk.co.gresearch.siembol.response.common.RespondingResult;
+import uk.co.gresearch.siembol.response.model.TimeExclusionEvaluatorAttributesDto;
+
+public class TimeExclusionEvaluatorFactory implements RespondingEvaluatorFactory {
+    private static final ObjectReader JSON_ATTRIBUTES_READER = new ObjectMapper()
+            .readerFor(TimeExclusionEvaluatorAttributesDto.class);
+    private final SiembolJsonSchemaValidator attributesSchema;
+
+    public TimeExclusionEvaluatorFactory() throws Exception {
+        attributesSchema = new SiembolJsonSchemaValidator(TimeExclusionEvaluatorFactory.class);
+    }
+
+    @Override
+    public RespondingResult createInstance(String attributes) {
+        try {
+            SiembolResult validationResult = attributesSchema.validate(attributes);
+            if (validationResult.getStatusCode() != SiembolResult.StatusCode.OK) {
+                return RespondingResult.fromSiembolResult(validationResult);
+            }
+            TimeExclusionEvaluatorAttributesDto attributesDto = JSON_ATTRIBUTES_READER.readValue(attributes);
+            var evaluator = new TimeExclusionEvaluator(attributesDto);
+            return RespondingResult.fromEvaluator(evaluator);
+        } catch (Exception e) {
+            return RespondingResult.fromException(e);
+        }
+    }
+
+    @Override
+    public RespondingResult getType() {
+        return RespondingResult.fromEvaluatorType(ProvidedEvaluators.TIME_EXCLUSION_EVALUATOR.toString());
+    }
+
+    @Override
+    public RespondingResult getAttributesJsonSchema() {
+        return RespondingResult.fromAttributesSchema(
+                attributesSchema.getJsonSchema().getAttributes().getJsonSchema());
+    }
+}
