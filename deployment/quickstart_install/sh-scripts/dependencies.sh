b1cf6e310aa0807dc5b02da9aa999a15f987a6a7
Siembol <siembol@Siembols-MacBook-Pro.local>
2022-07-07 08:42:17 +0100

    Adding monitoring + response

--- deployment/quickstart_install/sh-scripts/dependencies.sh
+++ deployment/quickstart_install/sh-scripts/dependencies.sh
@@ -7,14 +7,17 @@ helm repo add gresearch https://g-research.github.io/charts
 helm repo update
 
 helm install storm gresearch/storm -n=siembol \
-    --set supervisor.replicaCount=1  \
+    --set supervisor.replicaCount=2  \
     --set supervisor.childopts="-Xmx1g" \
     --set supervisor.slots=3 \
     --set zookeeper.fullnameOverride="siembol-zookeeper"
 
 helm install kafka bitnami/kafka -n=siembol \
     --set zookeeper.enabled=false \
-    --set externalZookeeper.servers={siembol-zookeeper-0.siembol-zookeeper-headless.siembol.svc}
+    --set externalZookeeper.servers={siembol-zookeeper-0.siembol-zookeeper-headless.siembol.svc} \
+    --set provisioning.enabled=true \
+    --set "provisioning.topics[0].name=siembol.alerts" \
+    --set "provisioning.topics[1].name=siembol.response.heartbeat"
 
 
 echo "************************************************************"
