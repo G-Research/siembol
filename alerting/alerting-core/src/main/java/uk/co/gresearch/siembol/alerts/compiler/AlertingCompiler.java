9844b75274fccd6cb4dfb90898f28cd7fb32c386
Marian Novotny <Marian.Novotny@gresearch.co.uk>
2022-10-21 16:49:16 +0100

    add alerting compiler docs

--- alerting/alerting-core/src/main/java/uk/co/gresearch/siembol/alerts/compiler/AlertingCompiler.java
+++ alerting/alerting-core/src/main/java/uk/co/gresearch/siembol/alerts/compiler/AlertingCompiler.java
@@ -28,7 +28,7 @@ import static uk.co.gresearch.siembol.alerts.common.AlertingResult.StatusCode.OK
  */
 public interface AlertingCompiler {
     /**
-     * Compile rules into alerting engine
+     * Compiles rules into alerting engine
      *
      * @param rules json string with alerting rules
      * @param logger logger for debugging
@@ -39,7 +39,7 @@ public interface AlertingCompiler {
     AlertingResult compile(String rules, TestingLogger logger);
 
     /**
-     * Compile list of rules into alerting engine
+     * Compiles list of rules into alerting engine
      *
      * @param rulesList list of json strings with alerting rules
      * @param logger logger for debugging
@@ -66,43 +66,43 @@ public interface AlertingCompiler {
         return new AlertingResult(OK, attributes);
     }
 
+    /**
+     * Provides json schema validator for alerting rules
+     *
+     * @return json schema validator for alerting rules
+     * @see JsonSchemaValidator
+     *
+     */
     JsonSchemaValidator getSchemaValidator();
 
+    /**
+     * Wraps an alerting rule into json rules structure
+     *
+     * @param rule json string with alerting rule
+     * @return json string with rules that contain the rule on input
+     * @throws IOException is rule is not valid json alerting rule
+     *
+     */
     String wrapRuleToRules(String rule) throws IOException;
 
+    /**
+     * Compiles rules into alerting engine and evaluate an event using the engine
+     *
+     * @param rules json string with alerting rules
+     * @param event string json for evaluation
+     * @return alerting result with testing result
+     * @see AlertingResult
+     */
     AlertingResult testRules(String rules, String event);
 
-    default AlertingResult compile(String rules) {
-        return compile(rules, new InactiveTestingLogger());
-    }
-
-    default AlertingResult compile(List<String> rulesList) {
-        return compile(rulesList, new InactiveTestingLogger());
-    }
-
-    default AlertingResult getSchema() {
-        AlertingAttributes attributes = new AlertingAttributes();
-        attributes.setRulesSchema(getSchemaValidator().getJsonSchema().getAttributes().getJsonSchema());
-        return new AlertingResult(OK, attributes);
-    }
-
-    default AlertingResult validateRule(String rule) {
-        try {
-            String rules = wrapRuleToRules(rule);
-            return validateRules(rules);
-        } catch (Exception e) {
-            return AlertingResult.fromException(e);
-        }
-    }
-
-    default AlertingResult validateRules(String rules) {
-        try {
-            return compile(rules);
-        } catch (Exception e) {
-            return AlertingResult.fromException(e);
-        }
-    }
-
+    /**
+     * Compiles a rule into alerting engine and evaluate an event using the engine
+     *
+     * @param rule json string with an alerting rule
+     * @param event string json for evaluation
+     * @return alerting result with testing result
+     * @see AlertingResult
+     */
     default AlertingResult testRule(String rule, String event) {
         try {
             String rules = wrapRuleToRules(rule);
@@ -112,6 +112,81 @@ public interface AlertingCompiler {
         }
     }
 
+    /**
+     * Compiles rules into alerting engine
+     *
+     * @param rules json string with alerting rules
+     * @return alerting result with alerting engine
+     * @see AlertingResult
+     * @see AlertingEngine
+     */
+    default AlertingResult compile(String rules) {
+        return compile(rules, new InactiveTestingLogger());
+    }
+
+    /**
+     * Compiles list of rules into alerting engine
+     *
+     * @param rulesList list of json strings with alerting rules
+     * @return alerting result with alerting engines
+     * @see AlertingResult
+     * @see AlertingEngine
+     */
+    default AlertingResult compile(List<String> rulesList) {
+        return compile(rulesList, new InactiveTestingLogger());
+    }
+
+    /**
+     * Provides json schema for alerting rules
+     *
+     * @return AlertingResult with json schema for alerting rules
+     * @see AlertingResult
+     *
+     */
+    default AlertingResult getSchema() {
+        AlertingAttributes attributes = new AlertingAttributes();
+        attributes.setRulesSchema(getSchemaValidator().getJsonSchema().getAttributes().getJsonSchema());
+        return new AlertingResult(OK, attributes);
+    }
+
+    /**
+     * Validates rule by trying to compile them
+     *
+     * @param rule json string with an alerting rule
+     * @return alerting result with status OK if the rule was able to compile
+     * @see AlertingResult
+     */
+    default AlertingResult validateRule(String rule) {
+        try {
+            String rules = wrapRuleToRules(rule);
+            return validateRules(rules);
+        } catch (Exception e) {
+            return AlertingResult.fromException(e);
+        }
+    }
+
+    /**
+     * Validates rules by trying to compile them
+     *
+     * @param rules json string with alerting rules
+     * @return alerting result with status OK if rules were able to compile
+     * @see AlertingResult
+     */
+    default AlertingResult validateRules(String rules) {
+        try {
+            return compile(rules);
+        } catch (Exception e) {
+            return AlertingResult.fromException(e);
+        }
+    }
+
+    /**
+     * Validates syntax of rules using json schema validator
+     *
+     * @param rules json string with alerting rules
+     * @return alerting result with status OK if rules have valid syntax
+     * @see AlertingResult
+     */
     default AlertingResult validateRulesSyntax(String rules) {
         try {
             SiembolResult validationResult = getSchemaValidator().validate(rules);
